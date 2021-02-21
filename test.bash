8cacbb05cef769cb9980d7d191aeca0def2e5b1c
root <root@saptami0072c.mylabserver.com>
2021-02-21 06:45:49 +0000

    new

--- test.bash
+++ test.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "show all tables"
-/usr/bin/mysql -ujenkins -p'Iamhere1@' 'mysql' < 'test.sql'
+#/usr/bin/mysql -ujenkins -p'Iamhere1@' 'mysql' < 'test.sql'
+find /var/lib/jenkins/workspace/op/*.sql -type f -mmin -10 -ls | awk '{print $11}' >> a.out
+while read line;
+ do
+         /usr/bin/mysql -ujenkins -p'Iamhere1@' 'mysql' < '$line'
+          echo $line;
+ done < a.out
+
--- test1.sql
+++ test1.sql
@@ -1,1 +1,2 @@
-insert into test2(id) values(1003);
+insert into test2(id) values(1005);
+insert into test2(id) values(10885);
